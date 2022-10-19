86f6848e61036355cb8578587d5b0a39523f3c79
Espen Moe-Nilssen <espen@medialog.no>
2022-10-19 15:36:56 +0200

    Adding project

--- src/acentoweb/cve/views/cve_view.py
+++ src/acentoweb/cve/views/cve_view.py
@@ -3,6 +3,8 @@
 from acentoweb.cve import _
 from Products.Five.browser import BrowserView
 
+import datetime;
+
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -13,5 +15,12 @@ class CveView(BrowserView):
 
     def __call__(self):
         # Implement your own actions:
-        self.msg = _(u'A small message')
+        #self.msg = _(u'A small message')
+        #We could put items and date here, might save a few milliseconds :)
         return self.index()
+
+    def get_items(self):
+        return self.context.portal_catalog(portal_type='CNLSE Glosa', sort_on='sortable_title', sort_order='ascending')
+
+    def now_date(self):
+        datetime.datetime.now().isoformat()
