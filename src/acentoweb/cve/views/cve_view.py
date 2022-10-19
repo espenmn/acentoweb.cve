dcfec98a3d923db45c7a299c9477285ccc695388
Espen Moe-Nilssen <espen@medialog.no>
2022-10-19 16:59:57 +0200

    Make python view with rolemap

--- src/acentoweb/cve/views/cve_view.py
+++ src/acentoweb/cve/views/cve_view.py
@@ -4,23 +4,52 @@ from acentoweb.cve import _
 from Products.Five.browser import BrowserView
 
 import datetime;
+from tempfile import TemporaryFile
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class CveView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('cve_view.pt')
+    #def __init__(self, context, request):
+    #    super(CveView, self).__init__(context, request)
 
     def __call__(self):
-        # Implement your own actions:
-        #self.msg = _(u'A small message')
-        #We could put items and date here, might save a few milliseconds :)
-        return self.index()
+        '''Returns the csv file,
+        '''
+        #We could put get items here, might save a few milliseconds :)
+        #return self.index()
+        request = self.request
+        context = self.context
+
+        CVE = """<?xml version="1.0" ?>
+<CV_RESOURCE AUTHOR="" DATE="%s" VERSION="0.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.mpi.nl/tools/elan/EAFv2.8.xsd">\n
+<LANGUAGE LANG_DEF="http://cdb.iso.org/lg/CDB-00138502-001" LANG_ID="eng" LANG_LABEL="English (eng)"/>
+<CONTROLLED_VOCABULARY CV_ID="ASL Signbank lexicon">
+<DESCRIPTION LANG_REF="eng">The main dataset in the ASL Signbank</DESCRIPTION>""" % datetime.datetime.now().isoformat()
+
+        for item in self.get_items():
+            #If we add cve_id to index, we can skip getObject for next line
+            obj = item.getObject()
+
+            CVE = CVE + "<CV_ENTRY_ML CVE_ID='{id}' EXT_REF='signbank-ecv'><CVE_VALUE DESCRIPTION='{description}' LANG_REF='eng'>{title}</CVE_VALUE></CV_ENTRY_ML>".format(id =obj.cve_id, description=obj.Description(), title=obj.Title())
+
+        CVE = CVE + """</CONTROLLED_VOCABULARY>
+<EXTERNAL_REF EXT_REF_ID="signbank-ecv" TYPE="resource_url"
+ VALUE="https://aslsignbank.haskins.yale.edu//dictionary/gloss/"/>
+</CV_RESOURCE>"""
+
+
+        # Add header
+
+        dataLen = len(CVE)
+        R = self.request.RESPONSE
+        R.setHeader('Content-Length', dataLen)
+        R.setHeader('Content-Type', 'text/cve')
+        R.setHeader('Content-Disposition', 'attachment; filename=%s.cve' % self.context.getId())
+
+        #return and downloads the file
+        return CVE
+
 
     def get_items(self):
-        return self.context.portal_catalog(portal_type='CNLSE Glosa', sort_on='sortable_title', sort_order='ascending')
-
-    def now_date(self):
-        datetime.datetime.now().isoformat()
+        return self.context.portal_catalog(portal_type=['CNLSE Glosa', 'cnlse_glosa'], sort_on='sortable_title', sort_order='ascending')
