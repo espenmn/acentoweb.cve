4f40ee4ba860b473de678125acb50c055d2fa566
Espen Moe-Nilssen <espen@medialog.no>
2023-02-20 18:48:41 +0100

    Added check for empty values and loop for all fields for content items schema

--- src/acentoweb/ecv/views/lexicon.py
+++ src/acentoweb/ecv/views/lexicon.py
@@ -4,10 +4,84 @@ from acentoweb.ecv import _
 from Products.Five.browser import BrowserView
 import datetime;
 from tempfile import TemporaryFile
-from zope.interface.interfaces import IMethod
+#from zope.interface.interfaces import IMethod
+from zope.schema import getFieldsInOrder
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import getUtility
 
 
-class Lexicon(BrowserView):
+
+
+
+
+class LexiconDisplay(BrowserView):
+
+    def __call__(self):
+        """Returns the ecv content,
+        """
+        #We could put get items here, might save a few milliseconds :)
+        #return self.index()
+        request = self.request
+        context = self.context
+
+        CVE = """<?xml version="1.0" encoding="UTF-8"?>
+<lexicon id="Demo Lexicon">"""
+
+        for item in self.get_items():
+            #If we add ecv_id to index, we can skip getObject for next line
+            obj = item.getObject()
+            eco_id = obj.cve_id.replace("\"", "\'")
+            eco_description = obj.Description().replace("\"", "\'")
+            eco_title = obj.Title().replace("\"", "\'")
+
+            #import pdb; pdb.set_trace()
+
+
+            CVE = CVE + """
+            <entry id="{id}">
+            <form>{title}</form>
+                <sense>
+                   <definition>{description}</definition>
+""".format(id=eco_id, description=eco_description, title=eco_title)
+
+            for key, value in getFieldsInOrder(obj.getTypeInfo().lookupSchema()):
+                value = getattr(getattr(obj, key), 'output', getattr(obj, key))
+
+                ## what shall we do with empty values?
+                if value == None or value == []:
+                    value = ""
+
+                # Alternatively, just skip it completely if not present
+                #if value != None and value != []::
+                CVE = CVE + """<{key}>{value}</{key}>""".format(value=value, key=key)
+
+        CVE = CVE + """</sense>
+    </entry>
+</lexicon>"""
+
+
+        dataLen = len(CVE)
+        R = self.request.RESPONSE
+        R.setHeader("Content-Length", dataLen)
+        R.setHeader("Content-Type", "text/exml")
+        self.request.RESPONSE.setHeader("Content-type", "text/xml")
+
+
+        #return xml
+
+        return CVE
+
+
+    def get_items(self):
+        return self.context.portal_catalog(portal_type=["CNLSE Glosa", "cnlse_glosa", "cnlse_gloss"], sort_on="sortable_title", sort_order="ascending")
+
+
+
+
+
+
+
+class LexiconView(BrowserView):
 
     def __call__(self):
         """Returns the ecv content,
@@ -17,7 +91,7 @@ class Lexicon(BrowserView):
 
 
         CVE = """<?xml version="1.0" encoding="UTF-8"?>
-                <lexicon id="Demo Lexicon">"""
+<lexicon id="Demo Lexicon">"""
 
         for item in self.get_items():
             #If we add ecv_id to index, we can skip getObject for next line
@@ -26,16 +100,19 @@ class Lexicon(BrowserView):
             eco_description = obj.Description().replace("\"", "\'")
             eco_title = obj.Title().replace("\"", "\'")
 
+
             CVE = CVE + """
             <entry id="{id}">
             <form>{title}</form>
                 <sense>
-                    <pos>{semantico}</pos>
+                    <pos>{semantic}</pos>
                     <gloss>gloss here</gloss>
                     <definition>{description}</definition>
+                    <gloss_id_en>{gloss_id_en}</gloss_id_en>
+                    <synonyms>{synonyms}</synonyms>
                 </sense>
             </entry>
-            """.format(id=eco_id, description=eco_description, title=eco_title, semantic=obj.semantico or None)
+""".format(id=eco_id, description=eco_description, title=eco_title, semantic=obj.semantic or None, gloss_id_en=obj.gloss_id_en, synonyms = obj.synonyms)
 
         CVE = CVE + """</lexicon>"""
 
@@ -45,7 +122,7 @@ class Lexicon(BrowserView):
         R = self.request.RESPONSE
         R.setHeader("Content-Length", dataLen)
         R.setHeader("Content-Type", "text/ecv")
-        R.setHeader("Content-Disposition", "attachment; filename=%s.ecv" % self.context.getId())
+        R.setHeader("Content-Disposition", "attachment; filename=%s.lexicon" % self.context.getId())
 
         #return and downloads the file
         return CVE
