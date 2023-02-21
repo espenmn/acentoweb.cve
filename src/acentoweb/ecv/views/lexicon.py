80381c804e1bb613dfb87f9284f40c36f5493378
Espen Moe-Nilssen <espen@medialog.no>
2023-02-21 12:53:29 +0100

    download link for images in lexicon

--- src/acentoweb/ecv/views/lexicon.py
+++ src/acentoweb/ecv/views/lexicon.py
@@ -4,14 +4,62 @@ from acentoweb.ecv import _
 from Products.Five.browser import BrowserView
 import datetime;
 from tempfile import TemporaryFile
-#from zope.interface.interfaces import IMethod
 from zope.schema import getFieldsInOrder
-from plone.dexterity.interfaces import IDexterityFTI
-from zope.component import getUtility
+#from zope.interface.interfaces import IMethod
+#from plone.dexterity.interfaces import IDexterityFTI
+#from zope.component import getUtility
 
+def getCVE(self):
+    """Returns the ecv content,
+    """
+    request = self.request
+    context = self.context
 
+    CVE = """<?xml version="1.0" encoding="UTF-8"?>
+<lexicon id="Demo Lexicon">"""
 
+    for item in self.context.portal_catalog(portal_type=["CNLSE Glosa", "cnlse_glosa", "cnlse_gloss"], sort_on="sortable_title", sort_order="ascending"):
+        #If we add ecv_id to index, we can skip getObject for next line
+        obj = item.getObject()
+        eco_id = obj.cve_id.replace("\"", "\'")
+        eco_description = obj.Description().replace("\"", "\'")
+        eco_title = obj.Title().replace("\"", "\'")
 
+        CVE = CVE + """
+        <entry id="{id}">
+        <form>{title}</form>
+            <sense>
+               <definition>{description}</definition>""".format(id=eco_id, description=eco_description, title=eco_title)
+
+        for key, value in getFieldsInOrder(obj.getTypeInfo().lookupSchema()):
+            value = getattr(getattr(obj, key), 'output', getattr(obj, key))
+
+            import pdb; pdb.set_trace()
+
+            ## what shall we do with empty/not existing values?
+            if type(value)() == []:
+                listvalues = ""
+                for ref_value in value:
+                    ## Do do: make a proper check for class
+                    if str(type(ref_value)) ==  "<class 'z3c.relationfield.relation.RelationValue'>":
+                        listvalues += ref_value.to_object.title
+                value=listvalues
+
+            ##If it is an image or file
+            if value.__class__.__name__ ==  "NamedBlobImage":
+                #If we want to download images, we probably need to redirect later or use a zip
+                value = "{url}/@@images/{key}".format(url = obj.absolute_url(), key=key)
+
+            if value == None or value == []:
+                value = ""
+
+            # Alternatively, just skip it completely if not present
+            #if value != None and value != []::
+            CVE = CVE + """<{key}>{value}</{key}>""".format(value=value, key=key)
+
+        CVE = CVE + """</sense></entry>"""
+    CVE = CVE + """</lexicon>"""
+    return CVE
 
 
 class LexiconDisplay(BrowserView):
@@ -24,40 +72,7 @@ class LexiconDisplay(BrowserView):
         request = self.request
         context = self.context
 
-        CVE = """<?xml version="1.0" encoding="UTF-8"?>
-<lexicon id="Demo Lexicon">"""
-
-        for item in self.get_items():
-            #If we add ecv_id to index, we can skip getObject for next line
-            obj = item.getObject()
-            eco_id = obj.cve_id.replace("\"", "\'")
-            eco_description = obj.Description().replace("\"", "\'")
-            eco_title = obj.Title().replace("\"", "\'")
-
-            #import pdb; pdb.set_trace()
-
-
-            CVE = CVE + """
-            <entry id="{id}">
-            <form>{title}</form>
-                <sense>
-                   <definition>{description}</definition>
-""".format(id=eco_id, description=eco_description, title=eco_title)
-
-            for key, value in getFieldsInOrder(obj.getTypeInfo().lookupSchema()):
-                value = getattr(getattr(obj, key), 'output', getattr(obj, key))
-
-                ## what shall we do with empty/not existing values?
-                if value == None or value == []:
-                    value = ""
-
-                # Alternatively, just skip it completely if not present
-                #if value != None and value != []::
-                CVE = CVE + """<{key}>{value}</{key}>""".format(value=value, key=key)
-
-        CVE = CVE + """</sense>
-    </entry>
-</lexicon>"""
+        CVE = getCVE(self)
 
 
         dataLen = len(CVE)
@@ -66,19 +81,8 @@ class LexiconDisplay(BrowserView):
         R.setHeader("Content-Type", "text/exml")
         self.request.RESPONSE.setHeader("Content-type", "text/xml")
 
-
         #return xml
-
         return CVE
-
-
-    def get_items(self):
-        return self.context.portal_catalog(portal_type=["CNLSE Glosa", "cnlse_glosa", "cnlse_gloss"], sort_on="sortable_title", sort_order="ascending")
-
-
-
-
-
 
 
 class LexiconView(BrowserView):
@@ -86,35 +90,12 @@ class LexiconView(BrowserView):
     def __call__(self):
         """Returns the ecv content,
         """
+        #We could put get items here, might save a few milliseconds :)
+        #return self.index()
         request = self.request
         context = self.context
 
-
-        CVE = """<?xml version="1.0" encoding="UTF-8"?>
-<lexicon id="Demo Lexicon">"""
-
-        for item in self.get_items():
-            #If we add ecv_id to index, we can skip getObject for next line
-            obj = item.getObject()
-            eco_id = obj.cve_id.replace("\"", "\'")
-            eco_description = obj.Description().replace("\"", "\'")
-            eco_title = obj.Title().replace("\"", "\'")
-
-
-            CVE = CVE + """
-            <entry id="{id}">
-            <form>{title}</form>
-                <sense>
-                    <pos>{semantic}</pos>
-                    <gloss>gloss here</gloss>
-                    <definition>{description}</definition>
-                    <gloss_id_en>{gloss_id_en}</gloss_id_en>
-                    <synonyms>{synonyms}</synonyms>
-                </sense>
-            </entry>
-""".format(id=eco_id, description=eco_description, title=eco_title, semantic=obj.semantic or None, gloss_id_en=obj.gloss_id_en, synonyms = obj.synonyms)
-
-        CVE = CVE + """</lexicon>"""
+        CVE = getCVE(self)
 
         # Add header
 
@@ -129,5 +110,5 @@ class LexiconView(BrowserView):
 
 
 
-    def get_items(self):
-        return self.context.portal_catalog(portal_type=["CNLSE Glosa", "cnlse_glosa", "cnlse_gloss"], sort_on="sortable_title", sort_order="ascending")
+#def get_items(self):
+#    return self.context.portal_catalog(portal_type=["CNLSE Glosa", "cnlse_glosa", "cnlse_gloss"], sort_on="sortable_title", sort_order="ascending")
